72124af1f5616e33b5829fd2fc5ec6bd13f1d2e0
Menyhárt Tamás <tomi04252002@gmail.com>
2023-09-19 18:55:21 +0200

    elrontott control c

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import './App.css';
-
-import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from './components/Login';
 
