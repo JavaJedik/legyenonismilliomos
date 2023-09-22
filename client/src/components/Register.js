8d287fb6ceb1e4466754c6558b280061a0aeea32
Pankotai Kristóf Géza <p.kristof1999@gmail.com>
2023-09-22 14:34:48 +0200

    Regisztrációs oldalon legördülő menü hozzáadása

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -10,54 +10,83 @@ const Register = () => {
         navigate("/login");
     };
 
+    function myFunction() {
+        document.getElementById("myDropdown").classList.toggle("show");
+    }
+
+    window.onclick = function(event) {
+        if (!event.target.matches('.dropbtn')) {
+            let dropdowns = document.getElementsByClassName("dropdown-content");
+            let i;
+            for (i = 0; i < dropdowns.length; i++) {
+                let openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    }
+
     return (
         <body>
         <div className = "reg-blur-container"></div>
-        <div className = "reg-container">
-            <div className = "content">
-                <input
-                    className= "input-field"
-                    type="text"
-                    placeholder="Felhasználónév"
-                    id="username"
-                />
+            <div className = "reg-container">
+                <div className = "content">
+                    <input
+                        className= "input-field"
+                        type="text"
+                        placeholder="Felhasználónév"
+                        id="username"
+                    />
+                </div>
+                <div className = "content">
+                    <input
+                        className= "input-field"
+                        type="text"
+                        placeholder="E-mail cím"
+                        id="email"
+                    />
+                </div>
+                <div className = "content">
+                    <input
+                        className= "input-field"
+                        type="password"
+                        placeholder="Jelszó"
+                        id="password"
+                    />
+                </div>
+                <div className = "content">
+                    <input
+                        className= "input-field"
+                        type="password"
+                        placeholder="Jelszó mégegyszer"
+                        id="password-check"
+                    />
+                </div>
+                <div className="dropdown">
+                    <button
+                        onClick={myFunction()}
+                        className="dropbtn">
+                        Adja meg a nemét!
+                    </button>
+                    <div id="myDropdown" className="dropdown-content">
+                        <p>1</p>
+                        <p>2</p>
+                        <p>3</p>
+                    </div>
+                </div>
+                <div className = "content">
+                    <button
+                        className= "button-style"
+                        onClick={navigateLogin}>
+                        Vissza
+                    </button>
+                    <button
+                        className= "button-style">
+                        Regisztráció
+                    </button>
+                </div>
             </div>
-            <div className = "content">
-                <input
-                    className= "input-field"
-                    type="text"
-                    placeholder="E-mail cím"
-                    id="email"
-                />
-            </div>
-            <div className = "content">
-                <input
-                    className= "input-field"
-                    type="password"
-                    placeholder="Jelszó"
-                    id="password"
-                />
-            </div>
-            <div className = "content">
-                <input
-                    className= "input-field"
-                    type="password"
-                    placeholder="Jelszó mégegyszer"
-                    id="password-check"
-                />
-            </div>
-            <div className = "content">
-                <button
-                    className= "button-style"
-                    onClick={navigateLogin}>
-                    Vissza
-                </button>
-                <button
-                    className= "button-style">
-                    Regisztráció
-                </button>
-            </div>
-        </div>
         </body>
     );
 };
