b36ed181e36c08ad2c30b42d54c02b3eef88f3ea
Menyhárt Tamás <tomi04252002@gmail.com>
2023-09-22 11:41:12 +0200

    home oldal

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import './App.css';
 import Quiz from "./components/Quiz";
+import Home from './components/Home';
 
 const App = () => {
   return (
     <BrowserRouter>
       <div>
         <Routes>
+          <Route path="/home" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/quiz" element={<Quiz />} />
         </Routes>
---
+++ client/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Üdvözöllek a Quiz alkalmazásban!</h1>
+      <p>Ez a kezdőoldal.</p>
+    </div>
+  );
+};
+
+export default Home;
